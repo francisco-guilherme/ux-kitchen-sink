2af632a80a0b05c9059748bfb0d601cf0650a383
Francisco Guilherme <francisco.guilherme@webdetails.pt>
2017-10-24 11:54:29 +0100

    Added initial components structure

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "./Header.jsx";
+import Content from "./Content.jsx";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Hitachi UX Kitchen Sink</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <Header />
+    <Content />
+  </div>
+);
+
+export default App;
---
+++ client/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <div className="header">
+    <div className="header-title">Header</div>
+  </div>
+);
+
+export default Header;
