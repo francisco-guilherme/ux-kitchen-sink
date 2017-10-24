373671ad37890976eb92fc64b8f674d289e701a9
Francisco Guilherme <francisco.guilherme@webdetails.pt>
2017-10-24 15:32:53 +0100

    Webpack config + base styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: "./client/index.html",
   filename: "index.html",
   inject: "body"
 });
 
+const ExtractCSS = new ExtractTextPlugin({
+  filename: "styles.min.css"
+});
+
 module.exports = {
-  entry: [
-    "./client/index.js"
-  ],
+  entry: ["./client/index.js", "./client/styles/main.scss"],
   output: {
-    path: path.resolve("dist"),
-    filename: "index_bundle.js"
+    path: path.resolve("static"),
+    filename: "index.min.js"
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.(css|scss)/,
+        use: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
+      }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig, ExtractCSS]
 };
+9
